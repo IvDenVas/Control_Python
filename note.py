90ba80217c8ddd31aa79ea47dbf1a68632db9207
Denis <ivdenvas@gmail.com>
2023-06-09 21:28:05 +0300

    added sort

--- note.py
+++ note.py
@@ -1,17 +1,20 @@
 import csv
 import datetime
 import os
+import operator
 
 def add():
     with open('file.csv', "a", newline="", encoding='utf-8') as file:
-        user = [get_id() + 1, input("Введите заголовок заметки: "), input("Введите тело новой заметки: "), datetime.datetime.now().strftime("%d-%m-%Y")]
+        user = [get_id() + 1, input("Введите заголовок заметки: "), input("Введите тело новой заметки: "), datetime.datetime.now().strftime("%Y-%m-%d")]
         writer = csv.writer(file, delimiter=";")
         writer.writerow(user)
 
 def show():
+    print("\nID - Тема заметки - Заметка - Дата создания/редактирования")
     with open('file.csv', newline="", encoding='utf-8') as file:
         read = csv.reader(file, delimiter=";")
-        for row in read:
+        sortedlist = sorted(read, key=operator.itemgetter(3), reverse=False)
+        for row in sortedlist:
             print(row[0], " - ", row[1], " - ", row[2], ' - ', row[3])
 
 def get_id():
@@ -23,6 +26,6 @@ def get_id():
                 max_id = int(row[0])
     return max_id
 
-add()
-#show()
+# add()
+show()
 #get_id()
