3cb345af0bb394e51950b3c373463b7984d5d24a
Denis <ivdenvas@gmail.com>
2023-06-09 12:13:22 +0300

    2 functions

--- note.py
+++ note.py
@@ -1,12 +1,18 @@
 import csv
+import datetime
 
-with open('file.csv', "a", newline="", encoding='utf-8') as file:
-    user = [input("Введите заголовок заметки: "), input("Введите тело новой заметки: ")]
-    writer = csv.writer(file)
-    writer.writerow(user)
+def add():
+    with open('file.csv', "a", newline="", encoding='utf-8') as file:
+        user = [input("Введите заголовок заметки: "), input("Введите тело новой заметки: "), datetime.datetime.now().strftime("%d-%m-%Y")]
+        writer = csv.writer(file)
+        writer.writerow(user)
+
+def show():
+    with open('file.csv', newline="", encoding='utf-8') as file:
+        read = csv.reader(file)
+        for row in read:
+            print(row[0], " - ", row[1])
 
 
-with open('file.csv', newline="", encoding='utf-8') as file:
-    read = csv.reader(file)
-    for row in read:
-        print(row[0], " - ", row[1])
+add()
+#show()
